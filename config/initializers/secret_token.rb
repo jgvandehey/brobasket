673e27a782e5b84baecb6001a520ca0049ec3e9b
Jeremy Vandehey <jgvandehey@gmail.com>
2015-01-14 13:58:58 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brobasket::Application.config.secret_token = '62bc323ea35524f567eb55e22a03bdceaa3fecba6c16e9c6b4e84f1bbee11306cf9f0a339503944af67e23894e02e110535f0b178e735c64b4c119042361b410'
